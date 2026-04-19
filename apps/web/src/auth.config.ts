470b427ec08c8dca73e3ea5e430a43b8b2b76ca1
drorganvidez <davromorg@alum.us.es>
2026-04-19 17:03:41 +0200

    fix: Fixes auth routes

--- apps/web/src/auth.config.ts
+++ apps/web/src/auth.config.ts
@@ -3,6 +3,7 @@ import type { NextAuthConfig } from "next-auth";
 export const authConfig: NextAuthConfig = {
   session: { strategy: "jwt" },
   pages: { signIn: "/login" },
+  trustHost: true,
   providers: [],
   callbacks: {
     authorized({ request, auth }) {
